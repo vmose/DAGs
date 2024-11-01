3ea12aa4b767cd2784a88197b9b969e9d06592c6
vmose <80821950+vmose@users.noreply.github.com>
2024-11-01 21:40:13 +0300

    provide_context not needed
    In recent versions of Airflow, provide_context is no longer required; Airflow automatically injects the context, including kwargs, if you define the function to accept **kwargs

--- XCom.py
+++ XCom.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+import logging
 
 # Default arguments for the DAG
 default_args = {
@@ -16,7 +17,7 @@ default_args = {
 dag = DAG(
     'example_etl_dag',
     default_args=default_args,
-    description='Twitter scrapping tag',
+    description='Twitter scraping DAG',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 1, 1),
     catchup=False,
@@ -24,47 +25,22 @@ dag = DAG(
 
 # Function to extract data
 def extract_data(**kwargs):
-    # Simulate data extraction
+    """Simulates data extraction."""
     extracted_data = {'data': [1, 2, 3, 4, 5]}
     kwargs['ti'].xcom_push(key='extracted_data', value=extracted_data)
-    print("Data extracted:", extracted_data)
+    logging.info("Data extracted: %s", extracted_data)
 
 # Function to transform data
 def transform_data(**kwargs):
+    """Transforms extracted data by doubling each value."""
     ti = kwargs['ti']
     extracted_data = ti.xcom_pull(key='extracted_data')
     transformed_data = [x * 2 for x in extracted_data['data']]
     ti.xcom_push(key='transformed_data', value=transformed_data)
-    print("Data transformed:", transformed_data)
+    logging.info("Data transformed: %s", transformed_data)
 
 # Function to load data
 def load_data(**kwargs):
+    """Loads transformed data (simulated)."""
     ti = kwargs['ti']
-    transformed_data = ti.xcom_pull(key='transformed_data')
-    # Simulate loading data
-    print("Data loaded:", transformed_data)
-
-# Define the tasks
-extract_task = PythonOperator(
-    task_id='extract_data',
-    python_callable=extract_data,
-    provide_context=True,
-    dag=dag,
-)
-
-transform_task = PythonOperator(
-    task_id='transform_data',
-    python_callable=transform_data,
-    provide_context=True,
-    dag=dag,
-)
-
-load_task = PythonOperator(
-    task_id='load_data',
-    python_callable=load_data,
-    provide_context=True,
-    dag=dag,
-)
-
-# Set the task dependencies
-extract_task >> transform_task >> load_task
+    transformed_data = ti.xcom_pull(key
